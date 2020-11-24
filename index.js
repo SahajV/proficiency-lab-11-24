b3a59312c96a55591ebe47bf2423e711d7a988cc
GildedBaboon <vedereysahaj1@gmail.com>
2020-11-24 14:30:22 -0500

    /movie gets random movie from db

--- index.js
+++ index.js
@@ -1,11 +1,42 @@
+const MongoClient = require("mongodb").MongoClient;
 const express = require("express");
 const app = express();
-const port = 8000;
+
+const dbName = "sample_mflix";
+const uri =
+  "mongodb+srv://dbUser:beimzy8xSTwp7iAJ@cluster0.byjdq.mongodb.net?w=majority";
+
+MongoClient.connect(
+  uri,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  (err, client) => {
+    if (err) {
+      console.log(err);
+      throw err;
+    }
+
+    console.log("Connected successfully to Mongodb");
+
+    const db = client.db(dbName);
+
+    app.locals.db = db;
+
+    const port = process.env.PORT || 8000;
+    app.listen(port, () => console.log(`Listening on port ${port}...`));
+  }
+);
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("whats popping");
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}!`);
+app.get("/movie", async (req, res) => {
+  const db = req.app.locals.db;
+
+  const movie = await db
+    .collection("movies")
+    .aggregate([{ $sample: { size: 1 } }])
+    .toArray();
+
+  res.send(movie);
 });
